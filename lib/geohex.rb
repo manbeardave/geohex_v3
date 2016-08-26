29031d3027decc17e09963fb2ebdfd3583c9ed2e
manbeardave <dave.hines@vervemobile.com>
2016-08-25 23:39:01 -0700

    initial push with lots of failing tests

--- lib/geohex.rb
+++ lib/geohex.rb
@@ -9,28 +9,27 @@ require 'ostruct'
 module GeoHex
   VERSION = '2.0.1'
 
-  H_KEY = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  H_BASE = 20037508.34;
-  H_DEG = Math::PI*(30.0/180);
-  H_K = Math.tan(H_DEG);
-  R=20037508.34
+  H_KEY = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" 
+  H_BASE = 20037508.34 
+  H_DEG = Math::PI*(30.0/180) 
+  H_K = Math.tan(H_DEG) 
+  R = 20037508.34
   
   class Zone
     attr_accessor :code, :lat, :lon, :x, :y
     def initialize(*params)
       raise ArgumentError, "latlng or code is needed" if params.size == 0
-      if params.first.is_a?(Float)
-        @lat,@lon = params[0],params[1]
-        @level= params[2]||7 
-        @code= GeoHex::Zone.encode(@lat,@lon,@level)
-      else
-        @code = params.first
-        @lat,@lon,@level=GeoHex::Zone.decode(@code)
-      end
+
+      @lat = params[0]
+      @lon = params[1]
+      @x   = params[2]
+      @y   = params[3]
+      @code= params[4]
+      
     end
     
     def self.calcHexSize(level)
-      H_BASE/(2**level)/3
+      H_BASE/(3**(level+3))
     end
     
     def level
@@ -41,20 +40,20 @@ module GeoHex
     end
     
     def hexCoords
-      h_lat = self.lat;
-      h_lon = self.lon;
-      h_xy = GeoHex::Zone.loc2xy(h_lon, h_lat);
+      h_lat = self.lat 
+      h_lon = self.lon 
+      h_xy = GeoHex::Zone.loc2xy(h_lon, h_lat) 
       h_x = h_xy.x
       h_y = h_xy.y
-      h_deg = Math.tan(Math::PI * (60 / 180));
+      h_deg = Math.tan(Math::PI * (60 / 180)) 
       h_size = self.hexSize
-      h_top = xy2loc(h_x, h_y + h_deg *  h_size).lat;
-      h_btm = xy2loc(h_x, h_y - h_deg *  h_size).lat;
+      h_top = xy2loc(h_x, h_y + h_deg *  h_size).lat 
+      h_btm = xy2loc(h_x, h_y - h_deg *  h_size).lat 
       
-      h_l = xy2loc(h_x - 2 * h_size, h_y).lon;
-      h_r = xy2loc(h_x + 2 * h_size, h_y).lon;
-      h_cl = xy2loc(h_x - 1 * h_size, h_y).lon;
-      h_cr = xy2loc(h_x + 1 * h_size, h_y).lon;
+      h_l = xy2loc(h_x - 2 * h_size, h_y).lon 
+      h_r = xy2loc(h_x + 2 * h_size, h_y).lon 
+      h_cl = xy2loc(h_x - 1 * h_size, h_y).lon 
+      h_cr = xy2loc(h_x + 1 * h_size, h_y).lon 
       
       [
        {:lat => h_lat, :lon => h_l},
@@ -78,19 +77,19 @@ module GeoHex
       z_xy = loc2xy(lon,lat)
       lon_grid = z_xy.x
       lat_grid = z_xy.y
-      unit_x = 6.0* h_size;
-      unit_y = 6.0* h_size*H_K;
-      h_pos_x = (lon_grid + lat_grid / H_K) / unit_x;
-      h_pos_y = (lat_grid - H_K*lon_grid) / unit_y;
+      unit_x = 6.0* h_size 
+      unit_y = 6.0* h_size*H_K 
+      h_pos_x = (lon_grid + lat_grid / H_K) / unit_x 
+      h_pos_y = (lat_grid - H_K*lon_grid) / unit_y 
 
-      h_x_0 = h_pos_x.floor;
-      h_y_0 = h_pos_y.floor;
+      h_x_0 = h_pos_x.floor 
+      h_y_0 = h_pos_y.floor 
       h_x_q = h_pos_x - h_x_0
       h_y_q = h_pos_y - h_y_0
-      h_x = h_pos_x.round;
-      h_y = h_pos_y.round;
+      h_x = h_pos_x.round 
+      h_y = h_pos_y.round 
       
-      h_max=(H_BASE/unit_x + H_BASE/unit_y).round;
+      h_max=(H_BASE/unit_x + H_BASE/unit_y).round 
       
       if ( h_y_q > -h_x_q + 1 ) 
         if ( h_y_q < (2 * h_x_q ) and  h_y_q > (0.5 * h_x_q ) )
@@ -104,116 +103,232 @@ module GeoHex
         end
       end
     
-      h_lat = (H_K * h_x * unit_x + h_y * unit_y) / 2;
-      h_lon = (h_lat - h_y * unit_y)/H_K;
+      h_lat = (H_K * h_x * unit_x + h_y * unit_y) / 2 
+      h_lon = (h_lat - h_y * unit_y)/H_K 
       
       z_loc = xy2loc(h_lon,h_lat)
       z_loc_x = z_loc.lon
       z_loc_y = z_loc.lat
       
       if (H_BASE - h_lon <h_size)
-        z_loc_x = 180.0;
-        h_xy = h_x;
-        h_x = h_y;
-        h_y = h_xy;
+        z_loc_x = 180.0 
+        h_xy = h_x 
+        h_x = h_y 
+        h_y = h_xy 
       end
       
-      h_x_p = (h_x<0) ? 1 : 0;
-      h_y_p = (h_y<0) ? 1 : 0;
-      h_x_abs = ((h_x).abs * 2 + h_x_p).to_f;
-      h_y_abs = ((h_y).abs * 2 + h_y_p).to_f;
-      h_x_10000 = ((h_x_abs%777600000)/12960000).floor;
-      h_x_1000 = ((h_x_abs%12960000)/216000).floor;
-      h_x_100 = ((h_x_abs%216000)/3600).floor;
-      h_x_10 = ((h_x_abs%3600)/60).floor;
-      h_x_1 = ((h_x_abs%3600)%60).floor;
-      h_y_10000 = ((h_y_abs%77600000)/12960000).floor;
-      h_y_1000 = ((h_y_abs%12960000)/216000).floor;
-      h_y_100 = ((h_y_abs%216000)/3600).floor;
-      h_y_10 = ((h_y_abs%3600)/60).floor;
-      h_y_1 = ((h_y_abs%3600)%60).floor;
-      h_code = H_KEY[level % 60, 1];
-      h_code += H_KEY[h_x_10000, 1]+H_KEY[h_y_10000, 1] if(h_max >=12960000/2) ;
-      h_code += H_KEY[h_x_1000, 1]+H_KEY[h_y_1000, 1] if(h_max >=216000/2) ;
-      h_code += H_KEY[h_x_100, 1]+H_KEY[h_y_100, 1] if(h_max >=3600/2) ;
-      h_code += H_KEY[h_x_10, 1]+H_KEY[h_y_10, 1] if(h_max >=60/2) ;
-      h_code += H_KEY[h_x_1, 1]+H_KEY[h_y_1, 1];
+      h_x_p = (h_x<0) ? 1 : 0 
+      h_y_p = (h_y<0) ? 1 : 0 
+      h_x_abs = ((h_x).abs * 2 + h_x_p).to_f 
+      h_y_abs = ((h_y).abs * 2 + h_y_p).to_f 
+      h_x_10000 = ((h_x_abs%777600000)/12960000).floor 
+      h_x_1000 = ((h_x_abs%12960000)/216000).floor 
+      h_x_100 = ((h_x_abs%216000)/3600).floor 
+      h_x_10 = ((h_x_abs%3600)/60).floor 
+      h_x_1 = ((h_x_abs%3600)%60).floor 
+      h_y_10000 = ((h_y_abs%77600000)/12960000).floor 
+      h_y_1000 = ((h_y_abs%12960000)/216000).floor 
+      h_y_100 = ((h_y_abs%216000)/3600).floor 
+      h_y_10 = ((h_y_abs%3600)/60).floor 
+      h_y_1 = ((h_y_abs%3600)%60).floor 
+      h_code = H_KEY[level % 60, 1] 
+      h_code += H_KEY[h_x_10000, 1]+H_KEY[h_y_10000, 1] if(h_max >=12960000/2)  
+      h_code += H_KEY[h_x_1000, 1]+H_KEY[h_y_1000, 1] if(h_max >=216000/2)  
+      h_code += H_KEY[h_x_100, 1]+H_KEY[h_y_100, 1] if(h_max >=3600/2)  
+      h_code += H_KEY[h_x_10, 1]+H_KEY[h_y_10, 1] if(h_max >=60/2)  
+      h_code += H_KEY[h_x_1, 1]+H_KEY[h_y_1, 1] 
       
-      return h_code;
+      return h_code 
     end
   
     # geohex to latlon
     def self.decode(code)
-      c_length = code.length;
-      level = H_KEY.index(code[0,1]);
-      scl = level;
-      h_size =  self.calcHexSize(level);
-      unit_x = 6.0 * h_size;
-      unit_y = 6.0 * h_size * H_K;
-      h_max = (H_BASE / unit_x + H_BASE / unit_y).round;
-      h_x = 0;
-      h_y = 0;
+      c_length = code.length 
+      level = H_KEY.index(code[0,1]) 
+      scl = level 
+      h_size =  self.calcHexSize(level) 
+      unit_x = 6.0 * h_size 
+      unit_y = 6.0 * h_size * H_K 
+      h_max = (H_BASE / unit_x + H_BASE / unit_y).round 
+      h_x = 0 
+      h_y = 0 
 
       if (h_max >= 12960000 / 2) 
         h_x = H_KEY.index(code[1,1]) * 12960000 + 
           H_KEY.index(code[3,1]) * 216000 + 
           H_KEY.index(code[5,1]) * 3600 + 
           H_KEY.index(code[7,1]) * 60 + 
-          H_KEY.index(code[9,1]);
+          H_KEY.index(code[9,1]) 
         h_y = H_KEY.index(code[2,1]) * 12960000 + 
           H_KEY.index(code[4,1]) * 216000 + 
           H_KEY.index(code[6,1]) * 3600 + 
           H_KEY.index(code[8,1]) * 60 + 
-          H_KEY.index(code[10,1]);
+          H_KEY.index(code[10,1]) 
       elsif (h_max >= 216000 / 2) 
         h_x = H_KEY.index(code[1,1]) * 216000 + 
           H_KEY.index(code[3,1]) * 3600 + 
           H_KEY.index(code[5,1]) * 60 + 
-          H_KEY.index(code[7,1]);
+          H_KEY.index(code[7,1]) 
         h_y = H_KEY.index(code[2,1]) * 216000 + 
           H_KEY.index(code[4,1]) * 3600 + 
           H_KEY.index(code[6,1]) * 60 + 
-          H_KEY.index(code[8,1]);
+          H_KEY.index(code[8,1]) 
       elsif (h_max >= 3600 / 2) 
         h_x = H_KEY.index(code[1,1]) * 3600 + 
           H_KEY.index(code[3,1]) * 60 + 
-          H_KEY.index(code[5,1]);
+          H_KEY.index(code[5,1]) 
 		h_y = H_KEY.index(code[2,1]) * 3600 + 
           H_KEY.index(code[4,1]) * 60 + 
-          H_KEY.index(code[6,1]);
+          H_KEY.index(code[6,1]) 
       elsif (h_max >= 60 / 2) 
         h_x = H_KEY.index(code[1,1]) * 60 + 
-          H_KEY.index(code[3,1]);
+          H_KEY.index(code[3,1]) 
         h_y = H_KEY.index(code[2,1]) * 60 + 
-          H_KEY.index(code[4,1]);
+          H_KEY.index(code[4,1]) 
       else
-        h_x = H_KEY.index(code[1,1]);
-        h_y = H_KEY.index(code[2,1]);
+        h_x = H_KEY.index(code[1,1]) 
+        h_y = H_KEY.index(code[2,1]) 
       end
-      h_x = (h_x % 2 == 1) ? -(h_x - 1) / 2 : h_x / 2;
-      h_y = (h_y % 2 == 1) ? -(h_y - 1) / 2 : h_y / 2;
+      h_x = (h_x % 2 == 1) ? -(h_x - 1) / 2 : h_x / 2 
+      h_y = (h_y % 2 == 1) ? -(h_y - 1) / 2 : h_y / 2 
 
-      h_lat_y = (H_K * h_x * unit_x + h_y * unit_y) / 2;
-      h_lon_x = (h_lat_y - h_y * unit_y) / H_K;
+      h_lat_y = (H_K * h_x * unit_x + h_y * unit_y) / 2 
+      h_lon_x = (h_lat_y - h_y * unit_y) / H_K 
 
-      h_loc = xy2loc(h_lon_x, h_lat_y);
+      h_loc = xy2loc(h_lon_x, h_lat_y) 
       return [h_loc.lat, h_loc.lon, level]
+    end
+    
+    def self.getZoneByXy(_x,_y, _level)
+
+    	h_size = self.calcHexSize(_level) 
+	
+    	h_x = _x
+    	h_y = _y
+      
+       
+
+    	unit_x = 6 * h_size
+    	unit_y = 6 * h_size * H_K
+
+    	h_lat = (H_K * h_x * unit_x + h_y * unit_y) / 2
+    	h_lon = (h_lat - h_y * unit_y) / H_K
+
+    	z_loc = self.xy2loc(h_lon, h_lat)
+    	z_loc_x = z_loc.lon
+    	z_loc_y = z_loc.lat
+ 
+    	max_hsteps = 3**(_level + 2)
+    	hsteps = (h_x - h_y).abs
+ 
+       
+      
+    
+    	if hsteps == max_hsteps
+    		if h_x > h_y
+      		tmp = h_x
+      		h_x = h_y
+      		h_y = tmp
+        end
+    		z_loc_x = -180
+      end
+      
+	     
+      
+    	h_code = ""
+       
+    	code3_x = Array.new
+    	code3_y = Array.new
+    	code3 = ""
+    	code9 = ""
+    	mod_x = h_x
+    	mod_y = h_y
+      
+       
+
+    	(0..(_level + 2)).each do |i| 
+    	  h_pow = 3**(_level + 2 - i)
+        
+    	  if mod_x >= (h_pow/2).ceil 
+    	    code3_x[i] =2 
+    	    mod_x -= h_pow 
+        elsif mod_x <= -(h_pow/2).ceil 
+    	    code3_x[i] =0 
+    	    mod_x += h_pow 
+        else
+    	    code3_x[i] =1 
+        end
+        
+         
+        
+    	  if mod_y >= (h_pow/2).ceil
+    	    code3_y[i] = 2 
+    	    mod_y -= h_pow 
+        elsif mod_y <= -(h_pow/2).ceil
+    	    code3_y[i] = 0 
+    	    mod_y += h_pow 
+    	  else
+    	    code3_y[i] = 1 
+        end
+        
+       
+      
+        
+    	  if i==2 && (z_loc_x==-180 || z_loc_x>=0) 
+    		  if code3_x[0] == 2 && code3_y[0] == 1 && code3_x[1] == code3_y[1] && code3_x[2] == code3_y[2] 
+      			code3_x[0] = 1
+      			code3_y[0] = 2
+          elsif code3_x[0] == 1 && code3_y[0] == 0 && code3_x[1] == code3_y[1] && code3_x[2] == code3_y[2] 
+      			code3_x[0] = 0
+      			code3_y[0] = 1
+          end
+        end
+      end
+      
+      
+       
+    	code3_x.each_with_index do |x,i|
+    	  code3  += "#{code3_x[i]}#{code3_y[i]}"
+    	  code9  += code3.to_i(3).to_s
+    	  h_code += code9
+    	  code9 = ""
+    	  code3 = ""
+      end
+      
+       
+    	h_2 = h_code[3..h_code.length]
+    	h_1 = h_code[0..3]
+    	h_a1 = (h_1.to_i/30).floor
+    	h_a2 = h_1.to_i%30
+      
+        
+    	h_code ="#{H_KEY[h_a1]}#{H_KEY[h_a2]}#{h_2}"
+
+    	# if GEOHEX.cache_on
+#         return _zoneCache[h_code]  if !_zoneCache[h_code]
+#
+#         return (_zoneCache[h_code] = new Zone(z_loc_y, z_loc_x, _x, _y, h_code)) 
+#       }else{
+     return GeoHex::Zone.new(z_loc_y, z_loc_x, _x, _y, h_code) 
+#       }
+
+      
     end
   end
     
   class << Zone
     def loc2xy(_lon,_lat) 
-      x=_lon*H_BASE/180;
-      y= Math.log(Math.tan((90+_lat)*Math::PI/360)) / (Math::PI / 180 );
-      y= y * H_BASE / 180;
-      return OpenStruct.new("x"=>x, "y"=>y);
+      x=_lon*H_BASE/180 
+      y= Math.log(Math.tan((90+_lat)*Math::PI/360)) / (Math::PI / 180 ) 
+      y= y * H_BASE / 180 
+      return OpenStruct.new("x"=>x, "y"=>y) 
     end
     
     def xy2loc(_x,_y) 
-      lon=(_x/H_BASE)*180;
-      lat=(_y/H_BASE)*180;
-    lat=180.0/Math::PI*(2.0*Math.atan(Math.exp(lat*Math::PI/180))-Math::PI/2);
-      return OpenStruct.new("lon" => lon,"lat" => lat);
+      lon=(_x/H_BASE)*180 
+      lat=(_y/H_BASE)*180 
+    lat=180.0/Math::PI*(2.0*Math.atan(Math.exp(lat*Math::PI/180))-Math::PI/2) 
+      return OpenStruct.new("lon" => lon,"lat" => lat) 
     end
   end
 end
--- spec/geohex_spec.rb
+++ spec/geohex_spec.rb
@@ -1,11 +1,14 @@
 require "#{File.expand_path(File.dirname(__FILE__))}/../lib/geohex.rb"
 require "pp"
+require "pry"
+require "csv"
 include GeoHex
 
 describe GeoHex do
   before(:all) do
     @test_ll2hex = []
     @test_hex2ll = []
+    @test_xy_to_hex = []
     File.open("#{File.expand_path(File.dirname(__FILE__))}/testdata_ll2hex.txt").read.each_line do |l|
       if l.slice(0,1) != "#"
         d = l.strip.split(',')
@@ -16,6 +19,12 @@ describe GeoHex do
       if l.slice(0,1) != "#"
         d = l.strip.split(',')
         @test_hex2ll << [d[0],d[1].to_f, d[2].to_f,d[3].to_i]
+      end
+    end
+    CSV.open("#{File.expand_path(File.dirname(__FILE__))}/fixture_getZoneByXY.csv").each do |l|
+      if l.slice(0,1) != "#"
+        d = l
+        @test_xy_to_hex << [d[0],d[1].to_f, d[2].to_f,d[3]]
       end
     end
   end  
@@ -32,19 +41,21 @@ describe GeoHex do
   it "should convert coordinates to geohex code" do
     # correct answers (you can obtain this test variables from jsver_test.html )
     @test_ll2hex.each do |v|
-      GeoHex::Zone.encode(v[0],v[1],v[2]).should == v[3]
+      expect(GeoHex::Zone.encode(v[0],v[1],v[2])).to eq(v[3])
     end
     
   end
+  
+  
   it "should convert geohex to coordinates " do
     # correct answers (you can obtain this test variables from jsver_test.html )
     @test_hex2ll.each do |v|
-      GeoHex::Zone.decode(v[0]).should == [v[1],v[2],v[3]]
+      expect(GeoHex::Zone.decode(v[0])).to eq([v[1],v[2],v[3]])
     end
   end
 
   it "should return instance from coordinates " do
-    GeoHex::Zone.new(35.647401,139.716911,12).code.should == 'mbas1eT'
+    expect(GeoHex::Zone.new(35.647401,139.716911,12).code).to eq 'mbas1eT'
   end
 
   it "should raise error if instancing with nil data " do
@@ -53,8 +64,16 @@ describe GeoHex do
 
   it "should return instance from hexcode " do
     geohex = GeoHex::Zone.new('wwhnTzSWp')
-    geohex.lat.should == 35.685262361266446
-    geohex.lon.should == 139.76695060729983
-    geohex.level.should == 22
+    expect(geohex.lat).to eq 35.685262361266446
+    expect(geohex.lon).to eq 139.76695060729983
+    expect(geohex.level).to eq 22
+  end
+  
+  
+  it "should return code from XY" do
+    @test_xy_to_hex.each do |v|
+      expect(GeoHex::Zone.getZoneByXy(v[1],v[2],v[0].to_i).code).to eq(v[3])
+    end
+    
   end
 end
